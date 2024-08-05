a44c8aa3fcf7ec4ff6aa4ef3a249c9d558ad4959
chittaranjan24 <chittaranjanshit2@gmail.com>
2024-08-05 11:40:02 +0530

    First initialization

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {    
+    extend: {
+      colors: {
+        olive: '#808000',
+        lavender: '#E6E6FA',
+      },
+    },
+  },
+  plugins: [],
+}
+
